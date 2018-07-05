d99b94b73c29ae0e8070b60b3448103c4a510405
tmexcept <200809201230_2008@163.com>
2018-07-05 17:25:53 +0800

    求最长子回文字符串的长度添加java版耗时O(N)
    根据LeetCode中的版本修改过来的java版本的求最长回文子字符串，耗时O(N)的实现

--- ebook/code/java/chapter1/Palindrome.java
+++ ebook/code/java/chapter1/Palindrome.java
@@ -62,4 +62,52 @@ public class Palindrome
 		return true;
 	}
 
+
+
+//https://articles.leetcode.com/longest-palindromic-substring-part-ii/#comment-34709
+//求最长子回文字符串的长度
+    String preProcess(String s) {
+        int n = s.length();
+        if (n == 0) return "^$";
+        String ret = "^";
+        for (int i = 0; i < n; i++)
+            ret += "#" + s.substring(i, 1);
+
+        ret += "#$";
+        return ret;
+    }
+    String longestPalindrome(String s) {
+        String T = preProcess(s);
+        int n = T.length();
+        int[] P = new int[n];
+        int id = 0, mx = 0;
+        for (int i = 1; i < n-1; i++) {
+            int j = 2*id-i; // equals to j = id - (i-id)
+
+            P[i] = (mx > i) ? Math.min(mx-i, P[j]) : 0;//在上述的c预言者赋值为1，
+
+            // Attempt to expand palindrome centered at i
+            while (T.valueOf(i + 1 + P[i]).equals(T.valueOf(i - 1 - P[i])))//这里判断以i为中心的字符串，向两侧延伸，判断是否相同，相同则++
+                P[i]++;
+
+            // If palindrome centered at i expand past R,
+            // adjust center based on expanded palindrome.
+            if (i + P[i] > mx) {
+                id = i;
+                mx = i + P[i];
+            }
+        }
+
+        // Find the maximum element in P.
+        int maxLen = 0;
+        int centerIndex = 0;
+        for (int i = 1; i < n-1; i++) {
+            if (P[i] > maxLen) {
+                maxLen = P[i];
+                centerIndex = i;
+            }
+        }
+
+        return s.substring((centerIndex - 1 - maxLen)/2, maxLen);
+    }
 }
